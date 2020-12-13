36202c258e047e5737a66f5c48e9b05f7266423b
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2020-12-13 23:01:42 +0200

    Generated new migration who added title value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_12_195146) do
+ActiveRecord::Schema.define(version: 2020_12_13_205818) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_12_12_195146) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
 end
