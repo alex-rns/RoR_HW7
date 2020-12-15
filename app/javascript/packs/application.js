352ce05e93c4b926771d078d984c02c71c66cb30
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2020-12-15 15:34:07 +0200

    Connected bootstrap via pacage.json

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-// import "bootstrap"
-// import "bootstrap-sprockets"
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+import "bootstrap"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
