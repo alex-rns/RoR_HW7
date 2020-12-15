a0d59f47ace9c8d71c16faa2da923ad634531860
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2020-12-15 13:39:04 +0200

    Changed bootstrap import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
-
+// import "bootstrap"
+// import "bootstrap-sprockets"
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
