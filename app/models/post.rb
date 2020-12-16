430b6ff73adb0000d7d48849cdc7bfaa4a73a64d
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2020-12-16 10:52:20 +0200

    Added responsive table with media querries. Styled code.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: {maximum: 30}
   validates :content, presence: true, length: {minimum: 10}
   validates :name, presence: true
-  validates :image, url: { allow_blank: true }
+  validates :image, url: {allow_blank: true}
 end
