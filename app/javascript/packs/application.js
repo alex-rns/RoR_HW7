0febc307428062fbc4cab956e89b0365c6fd0d31
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2020-12-31 17:36:08 +0200

    Check bug activetext + heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-// import  "trix"
-// import "@rails/actiontext"
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -11,7 +10,8 @@ import "channels"
 import "bootstrap"
 import JQuery from 'jquery';
 window.$ = window.JQuery = JQuery;
-
+import  "trix"
+import "@rails/actiontext"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
